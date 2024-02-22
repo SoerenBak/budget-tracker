c6bc77fb86fec3a5ccd3b7ed57584d8555b2bc13
SoerenBak <fff56k@gmail.com>
2024-02-22 20:47:16 +0100

    Started

---
+++ src/components/Balance.vue
@@ -0,0 +1,15 @@
+<template>
+    <h4>Your balance</h4>
+    <h1 id="blance">{{ total }} DKK</h1>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps ({
+    total: {
+        type: Number,
+        required: true
+    },
+});
+</script>
